63e97d08c8873b8b3a0176e53f02920100008639
JRBucher <jbuch001@odu.edu>
2025-11-03 15:59:27 -0500

    Update save.php
    fixed ".-" to ".=" on line 21

--- save.php
+++ save.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $content .= format_ratings("Wings", $wings);
     $content .= format_ratings("Mac & Cheese", $mac);
     $content .= format_ratings("Dessert", $dessert);
-    $content .- "--------------------------\n";
+    $content .= "--------------------------\n";
 
     file_put_contents($filename, $content, FILE_APPEND | LOCK_EX);
 
